7cedcbddc8ac174abcbdd8b9c7bb53bab1aa8d68
huxaiphaeridris@gmail.com <l654321123>
2017-10-26 13:59:47 +0300

    Updating code

--- Droid/FacebookPageRenderer.cs
+++ Droid/FacebookPageRenderer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Android.App;
+using Android.Widget;
 using Diabetes.Droid;
+using Diabetes.localDB;
 using Diabetes.Main;
 using Newtonsoft.Json.Linq;
 using Xamarin.Auth;
@@ -15,40 +17,54 @@ namespace Diabetes.Droid
     {
         public FacebookPageRenderer()
         {
+            // this is a ViewGroup - so should be able to load an AXML file and FindView<>
+            var activity = this.Context as Activity;
 
-			// this is a ViewGroup - so should be able to load an AXML file and FindView<>
-			var activity = this.Context as Activity;
+            var auth = new OAuth2Authenticator(
+                clientId: "134161603894277", // your OAuth2 client id
+                scope: "", // The scopes for the particular API you're accessing. The format for this will vary by API.
+                authorizeUrl: new Uri("https://www.facebook.com/v2.10/dialog/oauth/"), // the auth URL for the service
+                redirectUrl: new Uri("https://www.facebook.com/connect/login_success.html")); // the redirect URL for the service
 
-			var auth = new OAuth2Authenticator(
-				clientId: "134161603894277", // your OAuth2 client id
-				scope: "", // The scopes for the particular API you're accessing. The format for this will vary by API.
-				authorizeUrl: new Uri("https://www.facebook.com/v2.10/dialog/oauth/"), // the auth URL for the service
-				redirectUrl: new Uri("https://www.facebook.com/connect/login_success.html")); // the redirect URL for the service
+            auth.Completed += async (sender, eventArgs) =>
+            {
+                if (eventArgs.IsAuthenticated)
+                {
+                    try
+                    {
+                        var accesstoken = eventArgs.Account.Properties["access_token"].ToString();
+                        var expiresIn = Convert.ToDouble(eventArgs.Account.Properties["expires_in"]);
+                        var expiryDate = DateTime.Now + TimeSpan.FromSeconds(expiresIn);
+                        var request = new OAuth2Request("GET", new Uri("https://graph.facebook.com/me"), null, eventArgs.Account);
+                        var response = await request.GetResponseAsync();
+                        var obj = JObject.Parse(response.GetResponseText());
+                        var id = obj["id"].ToString().Replace("\"", "");
+                        var name = obj["name"].ToString().Replace("\"", "");
 
-			auth.Completed += async (sender, eventArgs) => {
-				if (eventArgs.IsAuthenticated)
-				{
-					var accesstoken = eventArgs.Account.Properties["access_token"].ToString();
-					var expiresIn = Convert.ToDouble(eventArgs.Account.Properties["expires_in"]);
-					var expiryDate = DateTime.Now + TimeSpan.FromSeconds(expiresIn);
-					var request = new OAuth2Request("GET", new Uri("https://graph.facebook.com/me"), null, eventArgs.Account);
-					var response = await request.GetResponseAsync();
-					var obj = JObject.Parse(response.GetResponseText());
+                        MedicationDatabase db = new MedicationDatabase();
+                       db.addUser(name, "true");
+                        // goes to the next form
+                        await App.NavigateToProfile(name);
+                    }
+                    catch (ArgumentOutOfRangeException el)
+					{
+						Toast.MakeText(activity, "Something went wrong , try again please ", ToastLength.Long).Show();
+					}
 
-					var id = obj["id"].ToString().Replace("\"", "");
-					var name = obj["name"].ToString().Replace("\"", "");
+					catch (Exception e)
+                    {
+                        Toast.MakeText(activity, "Something went wrong , try again please ", ToastLength.Long).Show();
+                    }
 
-					await App.NavigateToProfile(name);
+                }
+                else
+                {
+                    // The user cancelled
+                    await App.NavigateToProfile(string.Format("Sorry, you didn't login successfully, try again please"));
+                }
+            };
 
-				}
-				else
-				{
-					// The user cancelled
-					await App.NavigateToProfile(string.Format("Sorry, you didn't login successfully, try again please"));
-				}
-			};
-
-			activity.StartActivity(auth.GetUI(activity));
+            activity.StartActivity(auth.GetUI(activity));
 
 
         }
--- Droid/SetAlarmImplementation.cs
+++ Droid/SetAlarmImplementation.cs
@@ -2,6 +2,7 @@
 using Android.App;
 using Android.Content;
 using Diabetes.Droid;
+using Diabetes.localDB;
 using Diabetes.Main;
 
 [assembly: Xamarin.Forms.Dependency(typeof(SetAlarmImplementation))]
@@ -10,22 +11,34 @@ namespace Diabetes.Droid
     public class SetAlarmImplementation : ISetAlarm
     {
 
-        public void SetAlarm(int hour, int minute, string message,string title)
+        public void SetAlarm()
         {
-            Intent myintent = new Intent(Xamarin.Forms.Forms.Context, typeof(AlarmReceiver));
-			myintent.PutExtra("message", message);
-			myintent.PutExtra("title", title);
-            PendingIntent  pendingintent = PendingIntent.GetBroadcast(Xamarin.Forms.Forms.Context, 0, myintent, PendingIntentFlags.UpdateCurrent);
 
-            Java.Util.Date date = new Java.Util.Date();
-            Java.Util.Calendar cal = Java.Util.Calendar.Instance;
-            cal.TimeInMillis = Java.Lang.JavaSystem.CurrentTimeMillis();
-            cal.Set(Java.Util.CalendarField.HourOfDay, hour);
-            cal.Set(Java.Util.CalendarField.Minute, minute);
-            cal.Set(Java.Util.CalendarField.Second, 0);
-		//	PendingIntent pendingIntent = PendingIntent.GetBroadcast(this, 0, alarmIntent, PendingIntentFlags.UpdateCurrent);
-            AlarmManager alarmManager = Xamarin.Forms.Forms.Context.GetSystemService(Android.Content.Context.AlarmService) as AlarmManager;
-            alarmManager.Set(AlarmType.RtcWakeup, cal.TimeInMillis, pendingintent);
+			MedicationDatabase db = new MedicationDatabase();
+			var alarm_list = db.GetAlarmList();
+            //Debug.WriteLine(" Time -- : "+ m.ToString());
+            var title = "Diabetics App";
+            var message = "Hello  we remind you of taking your inslin dosage now";
+            foreach (var list in alarm_list)
+            {
+
+				var hour = Int32.Parse(list.Substring(0, 2));
+				var minute = Int32.Parse(list.Substring(3, 2));
+                Intent myintent = new Intent(Xamarin.Forms.Forms.Context, typeof(AlarmReceiver));
+                myintent.PutExtra("message", message);
+                myintent.PutExtra("title", title);
+                PendingIntent pendingintent = PendingIntent.GetBroadcast(Xamarin.Forms.Forms.Context, 0, myintent, PendingIntentFlags.UpdateCurrent);
+
+                Java.Util.Date date = new Java.Util.Date();
+                Java.Util.Calendar cal = Java.Util.Calendar.Instance;
+                cal.TimeInMillis = Java.Lang.JavaSystem.CurrentTimeMillis();
+                cal.Set(Java.Util.CalendarField.HourOfDay, hour);
+                cal.Set(Java.Util.CalendarField.Minute, minute);
+                cal.Set(Java.Util.CalendarField.Second, 0);
+                //	PendingIntent pendingIntent = PendingIntent.GetBroadcast(this, 0, alarmIntent, PendingIntentFlags.UpdateCurrent);
+                AlarmManager alarmManager = Xamarin.Forms.Forms.Context.GetSystemService(Android.Content.Context.AlarmService) as AlarmManager;
+                alarmManager.Set(AlarmType.RtcWakeup, cal.TimeInMillis, pendingintent);
+            }
         }
     }
 }
